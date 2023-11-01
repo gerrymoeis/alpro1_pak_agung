4239e21f35ca47341d084877443b5c0843e75b51
deaayunovitaputri <147597570+deaayunovitaputri@users.noreply.github.com>
2023-11-01 02:12:55 -0700

    Update tugas_alpro1.py

--- tugas_alpro1.py
+++ tugas_alpro1.py
@@ -10,3 +10,43 @@
 """
 
 toppings_pizza = ["Frankfurter BBQ", "Meat Monsta", "Super Supreme", "Super Supreme Chicken"]
+crusts_pizza = {
+    "Pan": {
+        "Personal": 43_637,
+        "Regular": 100_910,
+        "Large": 132_728
+    },
+    "StuffedCrust Cheese": {
+        "Personal": 55_455,
+        "Regular": 120_910,
+        "Large": 160_000
+    },
+    "StuffedCrust Sausage": {
+        "Personal": 52_728,
+        "Regular": 117_273,
+        "Large": 155_455
+    },
+    "Cheesy Bites": {
+        "Personal": 57_273,
+        "Regular": 123_637,
+        "Large": 164_546
+    },
+    "Crown Crust": {
+        "Personal": 55_455,
+        "Regular": 120_910,
+        "Large": 160_000
+    }
+}
+extra_cheese = {
+    "Personal": 13_636,
+    "Regular": 16_364,
+    "Large": 19_091
+}
+
+pizzas = {}
+for crust in crusts_pizza:
+    for size, cheese_price in extra_cheese.items():
+        crust_price = crusts_pizza[crust][size]
+        crusts_pizza[crust][size] = {"extra_cheese": cheese_price, "price": crust_price}
+for topping in toppings_pizza:
+    pizzas[topping] = crusts_pizza
